ab683081d731c9d69a565dd73fedc5e448eec9b5
Qs-F <qsf@de-liker.com>
2022-04-02 20:21:50 +0900

    Add display

--- src/app.tsx
+++ src/app.tsx
@@ -1,28 +1,38 @@
-import { } from 'react'
-import { Styles } from '/communication'
+import { Fragment } from 'react'
+import { StylesEvent } from '/communication'
 import { useEvent } from '/hooks/event'
 
 export const App = () => {
   const { event } = useEvent()
-  const styles = event?.data?.pluginMessage as Styles | undefined
+  const styles = event?.data?.pluginMessage as StylesEvent | undefined
   if (!styles) {
     return <p>Loading...</p>
   }
 
-  console.log('ui', styles)
+  console.log(styles)
 
   return (
     <ul>
       {
-        Object.entries(styles.color).map(([key, colors]) => (
-          <li key={key}>
-            <h2>{key}</h2>
+        Object.entries(styles.data.color).map(([styleKey, pages]) => (
+          <li key={styleKey}>
+            <h2>{styles.appendix.styles?.[styleKey]?.name}</h2>
             {
-              colors.map((v, id) => (
-                <div key={id}>
-                  <p>Page: { v.page.name }</p>
-                  <p>Node: { v.node.name }</p>
-                </div>
+              Object.entries(pages).map(([page, nodes], index) => (
+                <Fragment key={index}>
+                  <div>{
+                    styles.appendix.pages?.[page]?.name
+                  }</div>
+                  {
+                    nodes.map((node, id) => (
+                      <p key={id} onClick={() => {
+                        parent.postMessage({ pluginMessage: 'hello' })
+                      }}>Node: {
+                          styles.appendix.nodes?.[node]?.name
+                        }</p>
+                    ))
+                  }
+                </Fragment>
               ))
             }
           </li>
