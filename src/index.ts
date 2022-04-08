5aee64422c2007617668dc85e5b141875dbeb63e
Qs-F <qsf@de-liker.com>
2022-04-09 02:44:14 +0900

    Fix acc logic and Add focus on onclick handler

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
 import { Styles, StylesEvent } from '/communication'
 import { walk } from '/walk'
 
-// type Unit = {
-//   page: PageNode
-//   node: SceneNode
-// }
-
-// export const focus = ({ page, node }: Unit): void => {
-//   figma.currentPage = page
-//   page.selection = [node]
-// }
+const focus = (target: { page: string, node: string}): void => {
+  const page = figma.getNodeById(target.page) as PageNode
+  figma.currentPage = page
+  page.selection = [figma.getNodeById(target.node) as SceneNode]
+}
 
 // returns array of fille style id
 const fillFinder = (node: SceneNode): string[] => {
@@ -34,26 +30,26 @@ const getStyles = async (): Promise<StylesEvent> => {
       styles: {},
     },
   }
-  await Promise.all(figma.root.children.map(async (page) => {
+  await Promise.all(figma.root.children.map(async (page: PageNode) => {
     await walk(page, (node) => {
       if (node.type === 'PAGE' || node.type === 'DOCUMENT') {
         return true
       }
-      fillFinder(node).map((style) => {
+      fillFinder(node).map((style: string) => {
+        if (!style) return
         ret.appendix.pages[page.id] = { id: page.id, name: page.name }
         ret.appendix.nodes[node.id] = { id: node.id, name: node.name }
         const s = figma.getStyleById(style)
         !ret.appendix.styles[style] && s &&
           (ret.appendix.styles[style] = { id: s.id, name: s.name })
-        if (!styles.color[style]) {
+        if (!(style in styles.color)) {
           styles.color[style] = { [page.id]: [node.id] }
           return
         }
-        const t = styles.color[style] as Record<string, string[]>
-        if (!t[page.id]) {
-          t[page.id] = [node.id]
-          return
+        if (!(page.id in (styles.color[style] || {}))) {
+          Object.assign(styles.color[style], { [page.id]: [] })
         }
+        (styles.color[style]?.[page.id] || []).push(node.id) // TypeScript Hack
       })
       return true
     })
@@ -65,6 +61,7 @@ const main = async () => {
   figma.showUI(__html__, { visible: true })
   const styles = await getStyles()
   figma.ui.postMessage(styles)
+  figma.ui.onmessage = (message) => focus(message)
 }
 
 main()
